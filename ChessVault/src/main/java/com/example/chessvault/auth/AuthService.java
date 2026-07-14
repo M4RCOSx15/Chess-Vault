e3408b082a534ddd8c3183e542d222af5c7f0235
Marcos <marcos.gabriel.doth15@gmail.com>
2026-07-14 20:33:08 -0300

    fix: arrumando bagunça de pastas

--- ChessVault/src/main/java/com/example/chessvault/auth/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.example.chessvault.auth;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-}
--- ChessVault/src/main/java/com/example/chessvault/exception/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.chessvault.exception;
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
