7ff9aaa08af59c3e759f9eb8b2b9c89474eeb756
Marcos Gabriel <marcos.gabriel.doth15@gmail.com>
2026-09-21 19:58:02 -0300

    fix:UrlFront

--- ChessVault/ChessVault/src/main/java/com/example/chessvault/config/CorsConfig.java
+++ ChessVault/ChessVault/src/main/java/com/example/chessvault/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "https://45e3902d-chessvaultfrontend.chessvault.workers.dev/"
+                "https://chessvaultfrontend.chessvault.workers.dev/"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
