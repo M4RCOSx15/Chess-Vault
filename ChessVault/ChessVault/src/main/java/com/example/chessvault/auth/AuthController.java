d183f684a0371d93f8d2d8b178451229d2e6fe5d
Marcos Gabriel <marcos.gabriel.doth15@gmail.com>
2026-09-22 21:22:07 -0300

    Update CORS policy to allow multiple origins

--- ChessVault/ChessVault/src/main/java/com/example/chessvault/auth/AuthController.java
+++ ChessVault/ChessVault/src/main/java/com/example/chessvault/auth/AuthController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://chessvaultfrontend.chessvault.workers.dev/")
+@CrossOrigin(origins = {
+    "https://chessvaultfrontend.chessvault.workers.dev",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500"
+})
 public class AuthController {
 
     private final JwtService jwtService;
